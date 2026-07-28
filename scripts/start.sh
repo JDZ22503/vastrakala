166a684779ba3c2b787426a41ef6266831669f26
JDZ22503 <jenish22503@gmail.com>
2026-07-28 12:22:46 +0530

    Add public/storage images directly to git, remove storage:link

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,12 +9,8 @@ su www-data -s /bin/bash -c "php artisan route:cache"
 echo "Running migrations..."
 su www-data -s /bin/bash -c "php artisan migrate --force"
 
-echo "Creating storage symlink..."
-php artisan storage:link --force
-
-echo "Fixing permissions just in case..."
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-[ -e /var/www/html/public/storage ] && chown -h www-data:www-data /var/www/html/public/storage
+echo "Fixing permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public/storage
 
 echo "Starting Apache..."
 exec apache2-foreground
